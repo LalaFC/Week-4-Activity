a0af4cc1b045c9dcd5ce328281aa3be63f682556
OPS-DSKTP\MSI <angelik.operio@ciit.edu.ph>
2023-03-11 21:01:08 +0800

    Week 4 Activity v1

---
+++ Assets/EnemyMec.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyMec : MonoBehaviour
+{
+
+    private Vector2 Direction;
+    public GameObject bullet;
+    public Transform bulletPos;
+    private float timer = 4;
+
+    public float speed = 2;
+    public float GroundDist = 2;
+    public float EnemyRange = 4;
+    private bool _isRight;
+    public Transform Detector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Vector2 Movement =  Vector2.right * speed * Time.deltaTime;
+        transform.Translate(Movement);
+  
+
+        RaycastHit2D groundCheck = Physics2D.Raycast(Detector.position, Vector2.down, GroundDist);
+
+        if (groundCheck.collider == false) 
+        {
+            if (_isRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                _isRight = false;
+                Direction = Vector2.left;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                _isRight = true;
+                Direction = Vector2.right;
+            }
+        }
+        LazerBeam();
+    }
+
+    void shoot() 
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        timer = 0;
+
+
+    }
+
+    
+
+    public void LazerBeam()
+    {
+  
+            UnityEngine.Debug.DrawRay(Detector.position, (Direction * EnemyRange), Color.red, 1f);
+            var rays = Physics2D.RaycastAll(Detector.position, Direction, EnemyRange);
+
+            foreach (var ray in rays)
+            {
+                if (ray.collider == null)
+                    continue;
+
+                if (ray.collider.gameObject.CompareTag("Player") && (timer > 3))
+                {
+                    if (timer > 3)
+                    {
+                        shoot();
+                        UnityEngine.Debug.Log("Player Detected.");
+                    }
+                }
+            }
+
+    }
+
+}
